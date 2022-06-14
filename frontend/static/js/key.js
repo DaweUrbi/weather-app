782918580bd7afb02f7c435a62b57cb6b4bc8777
DaweUrbi <urban.da@icloud.com>
2022-06-14 08:54:09 -0700

    API key

---
+++ frontend/static/js/key.js
@@ -0,0 +1,5 @@
+const WEATHER_API_KEY = '75842e729462d3810db69c36af898f4b';
+export default { WEATHER_API_KEY };
+
+const PLACE_API_KEY = 'AIzaSyDcbVu38MCrM4_okhOMZdMTIViVO1Nojxk';
+export { PLACE_API_KEY };
